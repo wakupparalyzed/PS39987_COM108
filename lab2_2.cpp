870c1ba0d8977549c837891efc7e5f94cd9980af
wakupparalyzed <149561411+wakupparalyzed@users.noreply.github.com>
2023-11-07 02:53:39 +0700

    Update lab2_2.cpp

--- lab2_2.cpp
+++ lab2_2.cpp
@@ -2,14 +2,14 @@
 int main() {
 	int dai, rong;
 	printf("Hay nhap chieu dai: ");
-	scanf("%d",&dai);
+	scanf("%d", &dai);
 	printf("Hay nhap chieu rong: ");
-	scanf("%d",&rong);
+	scanf("%d", &rong);
 	int dienTich, chuVi;
-	dienTich = dai*rong;
-	chuVi = (dai+rong)*2;
-	printf("Dien tich hinh chu nhat la: %d\n",dienTich);
-	printf("Chu vi hinh chu nhat la : %d",chuVi);
+	dienTich = dai * rong;
+	chuVi = (dai + rong) * 2;
+	printf("Dien tich hinh chu nhat la: %d\n", dienTich);
+	printf("Chu vi hinh chu nhat la : %d", chuVi);
 	scanf("%s");
 	return 0;
 }
