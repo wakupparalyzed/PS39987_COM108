02a084e949d055024b4626d78fdb6176ae909547
wakupparalyzed <149561411+wakupparalyzed@users.noreply.github.com>
2023-11-15 12:49:05 +0700

    Create lab3_4.cpp

---
+++ lab3_4.cpp
@@ -0,0 +1,24 @@
+#include<stdio.h>
+
+int main() {
+	printf("bai 1: xay dung chuong trinh tinh hoc luc\n");
+	printf("bai 2: xay dung truong trinh giai phuong trinh\n");
+	printf("bai 3 :xay dung chuong trinh tinh tien dien\n");
+	
+	int luaChon;
+	printf("Chon di: ");
+	scanf("%d",&luaChon);
+	switch(luaChon) {
+		case 1:
+			printf("bai 1 xau dung chuong trinh hoc luc");
+			break;
+		case 2:
+			printf("bai 2: xay dung truong trinh giai phuong trinh");
+			break;
+		case 3:
+			printf(" bai 3 :xay dung chuong trinh tinh tienf dien");
+			break;
+		
+	}
+	return 0;
+}
