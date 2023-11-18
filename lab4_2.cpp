3141f5b4836f5065b796893417b3cdf4138cdc42
Thai <149561411+wakupparalyzed@users.noreply.github.com>
2023-11-18 23:07:50 +0700

    Create lab4_2.cpp

---
+++ lab4_2.cpp
@@ -0,0 +1,26 @@
+#include<stdio.h>
+
+int main() {
+	// Khai bao bien
+	int x;
+	
+	// input
+	printf("Hay nhap so tu ban phim: ");
+	scanf("%d, &x");
+	
+	// xu ly tinh toan - nhap 7
+	int dem=0; // day la so nguyen to
+	for(int i=2; i<x; i++) {
+		if(x%i==0) {
+			dem++; // day la so nguyen to
+		}
+	}
+	
+	// output
+	if(dem==0) {
+		printf("Day la so nguyen to");
+	} else {
+		printf("Day khong phai la so nguyen to");
+	}
+	return 0;
+}
