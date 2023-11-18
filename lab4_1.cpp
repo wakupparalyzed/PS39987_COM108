731f7ed8d672e748b8c502d8d7ae6324504b6e41
Thai <149561411+wakupparalyzed@users.noreply.github.com>
2023-11-18 23:01:02 +0700

    Create lab4_1.cpp

---
+++ lab4_1.cpp
@@ -0,0 +1,25 @@
+#include<stdio.h>
+int main() {
+	int min, max;
+	int tong = 0;
+	int bienDem = 0;
+  
+	printf("Nhap so tu nhien min: ");
+	scanf("%d", &min);
+	printf("Nhap so tu nhien max: ");
+	scanf("%d", &max);
+  
+	int i = min;
+  
+	while(i <= max) {
+		if(i % 2==0) {
+			tong +=i;
+			printf("%d\n", tong);
+			bienDem++;
+		}
+		i++;
+	}
+	int tbc = tong / bienDem;
+	printf("Trung binh tong cac so tu nhien chia het cho 2 tu min den max: %d", tbc);
+	return 0;
+}
