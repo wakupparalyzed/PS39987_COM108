8dee0b6f643e2b1088a13ff8ba7b35c610f970ef
Thai <149561411+wakupparalyzed@users.noreply.github.com>
2023-11-18 23:08:28 +0700

    Update lab4_1.cpp

--- lab4_1.cpp
+++ lab4_1.cpp
@@ -16,8 +16,7 @@ int main() {
 			tong +=i;
 			printf("%d\n", tong);
 			bienDem++;
-		}
-		i++;
+		} i++;
 	}
 	int tbc = tong / bienDem;
 	printf("Trung binh tong cac so tu nhien chia het cho 2 tu min den max: %d", tbc);
