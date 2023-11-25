1961468aa4dfd50aad0baf04a294b744e618a4f4
Thai <149561411+wakupparalyzed@users.noreply.github.com>
2023-11-25 20:11:58 +0700

    Add files via upload

---
+++ lab5_2.cpp
@@ -0,0 +1,23 @@
+#include<stdio.h>
+#include<conio.h>
+
+int checkNam(int nam) {
+	int check = 0;
+	if((nam % 400 == 0 || nam % 4 == 0) && (nam % 100 != 0)) {
+		check = 1;
+	}
+	return check;
+}
+
+int main() {
+	int year;
+	printf("Nhap vao nam: ");
+	scanf("%d", &year);
+	if(checkNam(year) == 1) {
+		printf("%d la nam nhuan", year);
+	} else {
+		printf("%d khong phai la nam nhuan", year);
+	}
+	
+	return 0;
+}
